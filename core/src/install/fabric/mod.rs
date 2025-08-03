aa11e75dc688b5dbe6853baa89a8636be2009cf9
Broken_Deer <64290747+Broken-Deer@users.noreply.github.com>
2025-08-04 01:23:00 +0800

    refactor(install): inline `install` logic into fabric/forge root modules (#131)

--- core/src/install/fabric/mod.rs
+++ core/src/install/fabric/mod.rs
@@ -2,12 +2,12 @@
 // Copyright 2022-2026 Broken-Deer and contributors. All rights reserved.
 // SPDX-License-Identifier: GPL-3.0-only
 
+use log::info;
 use serde::{Deserialize, Serialize};
 use serde_json::Value;
 use tauri_plugin_http::reqwest;
 
-pub mod install;
-pub use install::install;
+use crate::{folder::MinecraftLocation, version::Version};
 
 /// Represents a specific version of a Fabric artifact.
 #[derive(Deserialize, Serialize)]
@@ -110,4 +110,46 @@ pub struct LauncherMetaLibrariesItems {
     pub name: Option<String>,
     /// Optional URL to the library resource.
     pub url: Option<String>,
+}
+
+/// Downloads and saves the Fabric version metadata JSON file.
+///
+/// This function fetches the version metadata from the Fabric Meta API based on the specified
+/// Minecraft version and Fabric loader version. It saves the resulting `version.json`
+/// to the appropriate location inside the `.minecraft/versions` folder.
+///
+/// # Arguments
+///
+/// * `mcversion` - The target Minecraft version (e.g., `"1.20.1"`).
+/// * `fabric_version` - The loader version to be used (e.g., `"0.14.21"`).
+/// * `minecraft` - The local Minecraft installation location.
+///
+/// # Returns
+///
+/// Returns `Ok(())` if the file is successfully written, or an `anyhow::Error` if any step fails.
+///
+/// # Remarks
+///
+/// After calling this function, you should revalidate the libraries used by the version
+/// before launching the game, to ensure integrity and compatibility.
+pub async fn install(
+    mcversion: &str,
+    fabric_version: &str,
+    minecraft: MinecraftLocation,
+) -> anyhow::Result<()> {
+    info!("Saving version metadata file");
+    let url = format!(
+        "https://meta.fabricmc.net/v2/versions/loader/{mcversion}/{fabric_version}/profile/json"
+    );
+    let response = reqwest::get(url).await.unwrap();
+    let fabric_version_json: Version = response.json().await.unwrap();
+    let version_name = fabric_version_json.id.clone();
+    let json_path = minecraft.get_version_json(&version_name);
+    tokio::fs::create_dir_all(json_path.parent().unwrap()).await?;
+    tokio::fs::write(
+        json_path,
+        serde_json::to_string_pretty(&fabric_version_json).unwrap(),
+    )
+    .await?;
+    Ok(())
 }
