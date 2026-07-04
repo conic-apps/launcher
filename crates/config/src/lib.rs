3da3cd94d8853d5bc89cf118daa37bdaaf04bd98
OakChaser <64290747+OakChaser@users.noreply.github.com>
2026-07-04 15:15:51 +0800

    revert(setting): remove font family setting (#206)

--- crates/config/src/lib.rs
+++ crates/config/src/lib.rs
@@ -159,9 +159,6 @@ pub struct AppearanceConfig {
 
     /// Palette name, support mocha frappe latte macchiato.
     pub palette: String,
-
-    /// Font family for the launcher UI.
-    pub font_family: String,
 }
 
 impl Default for AppearanceConfig {
@@ -170,7 +167,6 @@ impl Default for AppearanceConfig {
         Self {
             palette_follow_system: true,
             palette: "Mocha".to_string(),
-            font_family: String::new(),
         }
     }
 }
