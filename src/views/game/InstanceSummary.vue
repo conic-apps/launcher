c66be07b33e7d0fbb6e00d0852fd726dc3cf008b
Broken_Deer <64290747+Broken-Deer@users.noreply.github.com>
2026-07-22 22:16:03 +0800

    feat: new app layout and design (#213)
    * feat: new app layout and design
    
    * update ci
    
    * lint

--- src/views/game/InstanceSummary.vue
+++ src/views/game/InstanceSummary.vue
@@ -1,10 +1,6 @@
 <template>
   <div class="current-instance">
     <div class="row-1">
-      <img
-        class="current-instance-icon"
-        :src="currentInstance.config.icon ?? instanceIconFallback"
-        alt="Instance icon" />
       <div class="current-instance-info">
         <!-- TODO: Click here to change Minecraft version -->
         <p class="title" v-if="currentInstance.id === LATEST_RELEASE_INSTANCE_ID">
@@ -14,29 +10,40 @@
           {{ $t("game.latestSnapshot") }}
         </p>
         <p class="title" v-else>{{ currentInstance.config.name }}</p>
-        <div class="env">
-          <p>
-            <span> Minecraft 版本 </span>
-            <span>{{ currentInstance.config.runtime.minecraft }}</span>
-          </p>
-          <div
-            class="line"
-            v-if="
-              currentInstance.config.runtime.mod_loader_type &&
-              currentInstance.config.runtime.mod_loader_version
-            "></div>
-          <p
-            v-if="
-              currentInstance.config.runtime.mod_loader_type &&
-              currentInstance.config.runtime.mod_loader_version
-            ">
-            <span> {{ currentInstance.config.runtime.mod_loader_type }} 版本 </span>
-            <span>{{ currentInstance.config.runtime.mod_loader_version }}</span>
-          </p>
-        </div>
       </div>
     </div>
     <div class="row-2">
+      <p>
+        <span> Minecraft 版本 </span>
+        <span>{{ currentInstance.config.runtime.minecraft }}</span>
+      </p>
+      <div
+        class="line"
+        v-if="
+          currentInstance.config.runtime.mod_loader_type &&
+          currentInstance.config.runtime.mod_loader_version
+        "></div>
+      <p
+        v-if="
+          currentInstance.config.runtime.mod_loader_type &&
+          currentInstance.config.runtime.mod_loader_version
+        ">
+        <span> {{ currentInstance.config.runtime.mod_loader_type }} 版本 </span>
+        <span>{{ currentInstance.config.runtime.mod_loader_version }}</span>
+      </p>
+      <div class="line"></div>
+      <p>
+        <span>最后运行日期</span>
+        <span>昨天</span>
+      </p>
+      <div class="line"></div>
+      <AppIcon name="time" :size="22" style="margin-right: 2px"></AppIcon>
+      <p>
+        <span>游戏时间</span>
+        <span>1032.2 小时</span>
+      </p>
+    </div>
+    <div class="row-3">
       <button class="launch-button">
         <AppIcon name="play" fill="#fff" style="margin-right: 4px"></AppIcon>
         开始游戏
@@ -49,18 +56,6 @@
           style="color: #fff"
           :size="16"></AppIcon>
       </button>
-      <div class="play-data">
-        <p>
-          <span>最后运行日期</span>
-          <span>昨天</span>
-        </p>
-        <div class="line"></div>
-        <AppIcon name="time" :size="22" style="margin-right: 2px"></AppIcon>
-        <p>
-          <span>游戏时间</span>
-          <span>1032.2 小时</span>
-        </p>
-      </div>
       <div class="actions">
         <button class="action-button">
           <AppIcon name="folder"></AppIcon>
@@ -73,12 +68,33 @@
         </button>
       </div>
     </div>
+    <div class="row-4">
+      <div>
+        <AppIcon name="save"></AppIcon>
+        <div><span class="type">存档</span><span class="count">1 个</span></div>
+      </div>
+      <div>
+        <AppIcon name="extension-puzzle" />
+        <div><span class="type">模组</span><span class="count">1 个</span></div>
+      </div>
+      <div>
+        <AppIcon name="folder" />
+        <div><span class="type">资源包</span><span class="count">4 个</span></div>
+      </div>
+      <div>
+        <AppIcon name="aperture-outline" />
+        <div><span class="type">光影包</span><span class="count">5 个</span></div>
+      </div>
+      <div>
+        <AppIcon name="images-outline" />
+        <div><span class="type">截图</span><span class="count">1 个</span></div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
 import AppIcon from "@/components/AppIcon.vue";
-import instanceIconFallback from "@/assets/images/Unknown_server.webp";
 import { useInstanceStore } from "@/store/instance";
 import { computed } from "vue";
 import { LATEST_RELEASE_INSTANCE_ID, LATEST_SNAPSHOT_INSTANCE_ID } from "@conic/instance";
@@ -91,8 +107,10 @@ const currentInstance = computed(() => {
 
 <style lang="less" scoped>
 .current-instance {
-  width: 100%;
-
+  position: absolute;
+  top: 40%;
+  transform: translateY(-50%);
+  margin-left: 48px;
   .row-1 {
     display: flex;
     align-items: center;
@@ -104,17 +122,21 @@ const currentInstance = computed(() => {
       background: var(--card-icon-background);
     }
     .current-instance-info {
-      padding-left: 16px;
       width: 100%;
       height: 100%;
       display: flex;
 
       .title {
-        font-size: 28px;
+        font-size: 38px;
       }
     }
   }
   .row-2 {
+    display: flex;
+    align-items: center;
+  }
+
+  .row-3 {
     display: flex;
     align-items: center;
     margin-top: 16px;
@@ -132,7 +154,7 @@ const currentInstance = computed(() => {
     }
     div.actions {
       display: flex;
-      margin-left: auto;
+      margin-left: 16px;
     }
     .action-button {
       appearance: none;
@@ -164,9 +186,9 @@ const currentInstance = computed(() => {
       appearance: none;
       border: none;
       color: #fff;
-      width: 114px;
-      height: 36px;
-      font-size: 14px;
+      width: 128px;
+      height: 42px;
+      font-size: 15px;
       display: flex;
       align-items: center;
       justify-content: center;
@@ -177,7 +199,7 @@ const currentInstance = computed(() => {
       appearance: none;
       border: none;
       background: #ffffff4f;
-      height: 36px;
+      height: 42px;
       width: 24px;
       border-radius: 0 8px 8px 0;
       background: rgb(114, 135, 253);
@@ -187,14 +209,10 @@ const currentInstance = computed(() => {
       opacity: 0.9;
     }
   }
-  .env,
-  .play-data {
-    margin-left: auto;
+  .row-2 {
     display: flex;
     align-items: center;
-    &:active {
-      opacity: 0.8;
-    }
+    margin-top: 16px;
     > p {
       font-size: 12px;
       display: flex;
@@ -218,8 +236,47 @@ const currentInstance = computed(() => {
       margin: 0px 8px;
     }
   }
-  .play-data {
-    margin-left: 16px;
+  .row-4 {
+    display: flex;
+    align-items: center;
+    margin-top: 16px;
+    background: #4f4f4f72;
+    background: var(--ctp-base);
+    backdrop-filter: blur(10px);
+    width: fit-content;
+    padding: 4px 4px;
+    border-radius: 12px;
+    > div {
+      display: flex;
+      align-items: center;
+      padding: 8px 16px;
+      border-radius: 8px;
+      transition: all 100ms ease;
+      &:hover {
+        background: var(--ctp-surface0);
+      }
+      &:active {
+        background: var(--ctp-surface1);
+        transform: scale(0.95);
+      }
+    }
+    > div > div {
+      font-size: 12px;
+      display: flex;
+      flex-direction: column;
+      align-items: initial;
+      width: fit-content;
+      padding: 2px 4px;
+      margin-left: 8px;
+      :first-child {
+        opacity: 0.8;
+        font-size: 12px;
+      }
+      :last-child {
+        margin-top: 2px;
+        font-size: 15px;
+      }
+    }
   }
 }
 </style>
